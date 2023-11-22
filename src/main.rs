c7bd03432c57e2d4591387564e1d4abed69b2d4a
imrany <imranmat254@gmail.com>
2023-11-23 00:22:06 +0300

    can now serve project through a command and file path

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,28 @@ use actix_files::{
     Files,
     NamedFile
 };
+use std::{
+    env::args,
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(||
+    let args: Vec<String>=args().collect();
+    // let flag=&args[1];
+    let file_path = args[1].clone();
+    println!("Running on http://localhost:8080");
+    HttpServer::new(move ||
         App::new()
-            .service(Files::new("/","/static_files").show_files_listing().index_file("index.html")
+            .service(Files::new("/",&file_path).show_files_listing().index_file("index.html")
                 .default_handler(fn_service(|req: ServiceRequest| async {
                     let (req, _) = req.into_parts();
                     let file = NamedFile::open_async("./static_files/404.html").await?;
                     let res = file.into_response(&req);
                     Ok(ServiceResponse::new(req, res))
                 }))
-            ))
-            .bind(("127.0.0.1",8080))?
-            .run()
-            .await
+            )
+    )
+    .bind(("127.0.0.1",8080))?
+    .run()
+    .await
 }
