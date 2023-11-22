7a0d3d403e2a19367a99549077b2802c129a22a5
imrany <imranmat254@gmail.com>
2023-11-23 01:06:58 +0300

    error handling -- display an error if user doesn't include path

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,20 @@ use actix_files::{
     NamedFile
 };
 use std::{
-    env::args,
+    env::{
+        args,
+    },
+    process,
 };
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args: Vec<String>=args().collect();
     // let flag=&args[1];
+    if args.len()<2 {
+        println!("Problem parsing arguments: Enter path \nexample: live_server /home/username/Desktop/static");
+        process::exit(1);
+    }
     let file_path = args[1].clone();
     println!("Running on http://localhost:8080");
     HttpServer::new(move ||
