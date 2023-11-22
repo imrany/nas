0b8a521893aae51172083d84ea36a235f1efab1d
imrany <imranmat254@gmail.com>
2023-11-22 20:11:19 +0300

    able to server directory in the project -- can access it through the url e.g '127.0.0.1:8080/static_files'

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
 use actix_web::{
     HttpServer,
-    HttpRequest,
-    Result,
     App,
-    get
 };
-use actix_files::NamedFile;
-use std::path::PathBuf;
-
-#[get("/{filename:.*}")]
-async fn server_static(req: HttpRequest)-> Result<NamedFile>{
-    let path: PathBuf=req.match_info().query("filename").parse().unwrap();
-    Ok(NamedFile::open(path)?)
-}
+use actix_files::Files;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(||{
+    HttpServer::new(||
         App::new()
-            .service(server_static)
-    })
+            .service(Files::new("/","/static_files").show_files_listing().index_file("/static_files/index.html")))
     .bind(("127.0.0.1",8080))?
     .run()
     .await
