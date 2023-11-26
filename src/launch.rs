cc5d4b73e989594054f03ff2794c8312190e7d4d
imrany <imranmat254@gmail.com>
2023-11-26 16:10:45 +0300

    try to add headless browser

--- src/launch.rs
+++ src/launch.rs
@@ -7,23 +7,26 @@ use anyhow::{
 };
 use async_std;
 
+// #[async_std::main]
 pub async fn launch_browser(url:&str)->Result<(), Box<dyn std::error::Error>>{
     // create a `Browser` that spawns a `chromium` process running with UI (`with_head()`, headless is default) 
    // and the handler that drives the websocket etc.
-   let (mut browser, mut handler) =
-   Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
+   let (browser, handler) =
+    Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
 
     // spawn a new task that continuously polls the handler
-    // let handle = async_std::task::spawn(async move {
-    // while let Some(h) = handler.next().await {
-    //     if h.is_err() {
-    //         break;
-    //     }
-    // }
-    // });
+    let handle = async_std::task::spawn(async move {
+        while let Some(h) = handler.next().await {
+            if h.is_err() {
+                break;
+            }
+        }
+    });
 
     // create a new browser page and navigate to the url
     let page = browser.new_page(url).await?;
+    page.wait_for_navigation().await?.content().await?;
 
+    handle.await;
     Ok(())
 }
