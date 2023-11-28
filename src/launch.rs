91b618570a9b6b9481044b308439125c04d89493
imrany <imranmat254@gmail.com>
2023-11-28 21:24:29 +0300

    able to launch browser on a seperate thread

--- src/launch.rs
+++ src/launch.rs
@@ -9,11 +9,11 @@ use futures::StreamExt;
 use async_std;
 
 // #[async_std::main]
-pub async fn launch_browser(url:&str)->Result<(), Box<dyn std::error::Error>>{
+pub async fn launch_browser(url:&str)->Result<(), std::io::Error>{
     // create a `Browser` that spawns a `chromium` process running with UI (`with_head()`, headless is default) 
    // and the handler that drives the websocket etc.
    let (browser, mut handler) =
-    Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
+    Browser::launch(BrowserConfig::builder().with_head().build().unwrap()).await.unwrap();
 
     // spawn a new task that continuously polls the handler
     let handle = async_std::task::spawn(async move {
@@ -23,9 +23,7 @@ pub async fn launch_browser(url:&str)->Result<(), Box<dyn std::error::Error>>{
     });
 
     // create a new browser page and navigate to the url
-    browser.new_page(url).await?;
-    // page.wait_for_navigation().await?.content().await?;
-
+    browser.new_page(url).await.unwrap();
     handle.await;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use actix_files::{
     NamedFile
 };
 use std::{
+    thread,
     env::{
         args,
         current_exe
@@ -23,7 +24,14 @@ mod launch;
 use launch::launch_browser;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(),std::io::Error> {
+    thread::spawn(||{
+        async_std::task::spawn(async move {
+            // launching browser
+            launch_browser("http://localhost:8080").await.unwrap_or_else(|err| println!("{}",err));
+        });
+    });
+
     let args: Vec<String>=args().collect();
     // let flag=&args[1];
     if args.len()<2 {
@@ -31,26 +39,29 @@ async fn main() -> std::io::Result<()> {
         process::exit(1);
     }
 
-    let launcher= launch_browser("http://localhost:8080").await;
-    match launcher {
-        Err(e) => println!("{}",e),
-        _ => println!("Browser launched\nServing on http://localhost:8080")
-    }
 
     let file_path = args[1].clone();
-    HttpServer::new(move ||
-        App::new()
-            .service(Files::new("/",&file_path).show_files_listing().index_file("index.html")
-                .default_handler(fn_service(|req: ServiceRequest| async {
-                    let (req, _) = req.into_parts();
-                    let current_exe_path=PathBuf::from(current_exe().unwrap());
-                    let file = NamedFile::open_async(Path::new(current_exe_path.parent().unwrap()).join("static_files/404.html")).await?;
-                    let res = file.into_response(&req);
-                    Ok(ServiceResponse::new(req, res))
-                }))
-            )
-    )
-    .bind(("127.0.0.1",8080))?
-    .run()
-    .await
+    let server=
+        HttpServer::new(move ||
+            App::new()
+                .service(Files::new("/",&file_path).show_files_listing().index_file("index.html")
+                    .default_handler(fn_service(|req: ServiceRequest| async {
+                        let (req, _) = req.into_parts();
+                        let current_exe_path=PathBuf::from(current_exe().unwrap());
+                        let file = NamedFile::open_async(Path::new(current_exe_path.parent().unwrap()).join("static_files/404.html")).await?;
+                        let res = file.into_response(&req);
+                        Ok(ServiceResponse::new(req, res))
+                    }))
+                )
+        )
+        .bind(("127.0.0.1",8080));
+
+    match server {
+        Ok(serve) => {
+            println!("Serving on http://localhost:8080");
+            serve.run().await.unwrap_or_else(|err| println!("{}",err));
+        },
+        Err(e) =>  println!("{}",e)
+    }
+    Ok(())
 }
