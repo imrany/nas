60088b733d336b3867a004e99d5f1f45f954cafa
imrany <imranmat254@gmail.com>
2023-11-29 15:53:45 +0300

    using open crate rather than chromiumoxide to launch browser -- this has reduce the build from 5.1mbs to 3.2mbs

--- src/launch.rs
+++ src/launch.rs
@@ -1,29 +1,9 @@
-use chromiumoxide::browser::{
-    Browser, 
-    BrowserConfig
-};
-use anyhow::{
-    Result,
-};
-use futures::StreamExt;
-use async_std;
+use open::that;
 
-// #[async_std::main]
 pub async fn launch_browser(url:&str)->Result<(), std::io::Error>{
-    // create a `Browser` that spawns a `chromium` process running with UI (`with_head()`, headless is default) 
-   // and the handler that drives the websocket etc.
-   let (browser, mut handler) =
-    Browser::launch(BrowserConfig::builder().with_head().build().unwrap()).await.unwrap();
-
-    // spawn a new task that continuously polls the handler
-    let handle = async_std::task::spawn(async move {
-        loop {
-            let _event = handler.next().await.unwrap();
-        }
-    });
-
-    // create a new browser page and navigate to the url
-    browser.new_page(url).await.unwrap();
-    handle.await;
-    Ok(())
+   match that(url) {
+    Ok(()) => println!("Serving on {}",url),
+    Err(e) => println!("An error occurred when opening {} : {}", url, e)
+   }
+   Ok(())
 }
