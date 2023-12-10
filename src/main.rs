d1c2a5fe0d18c6d54697c8f3fb427b64901d4f98
imrany <imranmat254@gmail.com>
2023-12-10 13:52:00 +0300

    researching on clap

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 use clap::Parser;
-use actix_web::{
-    HttpServer,
-    App,
-    dev::{
-        ServiceRequest, 
-        ServiceResponse, 
-        fn_service
-    }
-};
-use actix_files::{
-    Files,
-    NamedFile
-};
-use std::{
-    thread,
-    env::{
-        args,
-        current_exe
-    },
-    process,
-    path::{Path,PathBuf}
-};
-mod launch;
-use launch::launch_browser;
+// use actix_web::{
+//     HttpServer,
+//     App,
+//     dev::{
+//         ServiceRequest, 
+//         ServiceResponse, 
+//         fn_service
+//     }
+// };
+// use actix_files::{
+//     Files,
+//     NamedFile
+// };
+// use std::{
+//     thread,
+//     env::{
+//         args,
+//         current_exe
+//     },
+//     process,
+//     path::{Path,PathBuf}
+// };
+// mod launch;
+// use launch::launch_browser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -38,42 +38,48 @@ struct Args {
 
 #[actix_web::main]
 async fn main() -> Result<(),std::io::Error> {
-    thread::spawn(||{
-        async_std::task::spawn(async move {
-            // launching browser
-            launch_browser("http://localhost:8080").await.unwrap_or_else(|err| println!("{}",err));
-        });
-    });
+    let args = Args::parse();
 
-    let args: Vec<String>=args().collect();
-    // let flag=&args[1];
-    if args.len()<2 {
-        println!("Problem parsing arguments: Enter path \nexample: live_server /home/username/Desktop/static");
-        process::exit(1);
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
     }
 
+    // thread::spawn(||{
+    //     async_std::task::spawn(async move {
+    //         // launching browser
+    //         launch_browser("http://localhost:8080").await.unwrap_or_else(|err| println!("{}",err));
+    //     });
+    // });
 
-    let file_path = args[1].clone();
-    let server=
-        HttpServer::new(move ||
-            App::new()
-                .service(Files::new("/",&file_path).show_files_listing().index_file("index.html")
-                    .default_handler(fn_service(|req: ServiceRequest| async {
-                        let (req, _) = req.into_parts();
-                        let current_exe_path=PathBuf::from(current_exe().unwrap());
-                        let file = NamedFile::open_async(Path::new(current_exe_path.parent().unwrap()).join("static_files/404.html")).await?;
-                        let res = file.into_response(&req);
-                        Ok(ServiceResponse::new(req, res))
-                    }))
-                )
-        )
-        .bind(("0.0.0.0",8080));
+    // let args: Vec<String>=args().collect();
+    // // let flag=&args[1];
+    // if args.len()<2 {
+    //     println!("Problem parsing arguments: Enter path \nexample: live_server /home/username/Desktop/static");
+    //     process::exit(1);
+    // }
 
-    match server {
-        Ok(serve) => {
-            serve.run().await.unwrap_or_else(|err| println!("{}",err));
-        },
-        Err(e) =>  println!("{}",e)
-    }
+
+    // let file_path = args[1].clone();
+    // let server=
+    //     HttpServer::new(move ||
+    //         App::new()
+    //             .service(Files::new("/",&file_path).show_files_listing().index_file("index.html")
+    //                 .default_handler(fn_service(|req: ServiceRequest| async {
+    //                     let (req, _) = req.into_parts();
+    //                     let current_exe_path=PathBuf::from(current_exe().unwrap());
+    //                     let file = NamedFile::open_async(Path::new(current_exe_path.parent().unwrap()).join("static_files/404.html")).await?;
+    //                     let res = file.into_response(&req);
+    //                     Ok(ServiceResponse::new(req, res))
+    //                 }))
+    //             )
+    //     )
+    //     .bind(("0.0.0.0",8080));
+
+    // match server {
+    //     Ok(serve) => {
+    //         serve.run().await.unwrap_or_else(|err| println!("{}",err));
+    //     },
+    //     Err(e) =>  println!("{}",e)
+    // }
     Ok(())
 }
