7d3d0eb96213706e1f0b1fab6f922fff11b91131
imrany <imranmat254@gmail.com>
2023-12-14 18:36:46 +0300

    println!({url}) --instead of println!({}, url)

--- src/launch.rs
+++ src/launch.rs
@@ -2,8 +2,8 @@ use open::that;
 
 pub async fn launch_browser(url:&str)->Result<(), std::io::Error>{
    match that(url) {
-    Ok(()) => println!("Serving on {}",url),
-    Err(e) => println!("An error occurred when opening {} : {}", url, e)
+    Ok(()) => println!("Serving on {url}"),
+    Err(e) => println!("An error occurred when opening {url} : {e}")
    }
    Ok(())
 }
