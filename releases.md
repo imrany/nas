b053ebb55e66b9e1cd2d5c71a43a0a23dc65574b
imrany <imranmat254@gmail.com>
2023-12-16 10:13:55 +0300

    Network error patched --v1.0.1

--- releases.md
+++ releases.md
@@ -1,6 +1,14 @@
 # Releases
 
-## v1.0.0 - Recommended for most users
+## v1.0.1 - Recommended for most users
+
+| __Zippy versions__ | __Release Date__ | __Platforms__ | __Releases__ |
+|--------------------|------------------|---------------|--------------|
+| v1.0.1  | 2023-12-16 | Windows_x64 | [zippy-v1.0.1-windows-x64.zip](https://github.com/imrany/zippy/releases/download/v1.0.1/windows-x64.zip) |
+| v1.0.1  | 2023-12-16 | Linux_x64 | [zippy-v1.0.1-linux-x64.tar.gz](https://github.com/imrany/zippy/releases/download/v1.0.1/linux-x64.tar.gz) |
+
+
+## v1.0.0 - Not Maintained
 
 | __Zippy versions__ | __Release Date__ | __Platforms__ | __Releases__ |
 |--------------------|------------------|---------------|--------------|
--- src/launch.rs
+++ src/launch.rs
@@ -3,11 +3,16 @@ use colored::Colorize;
 use local_ip_address::local_ip;
 
 pub async fn launch_browser(url:&str)->Result<(), std::io::Error>{
-   let my_local_ip = local_ip().unwrap();
    match that(url) {
       Ok(()) => {
+         let my_local_ip = local_ip();
          println!(" Local: {}",format!("{url}").cyan());
-         println!(" Network: {}",format!("http://{my_local_ip:?}:8080/").cyan());
+         match my_local_ip {
+            Ok(ip) => {
+               println!(" Network: {}",format!("http://{ip}:8080/").cyan());
+            },
+            Err(e) => println!(" {} {}.", format!(" WARNING ").on_yellow().color("white"),e )
+         }
       },
       Err(_) => println!(" {} An error occurred when opening {}", " ERROR ".on_red().color("white"),format!("{url}").cyan())
    }
