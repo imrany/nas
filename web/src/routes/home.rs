050779594b984551f31cf567dbcfb4ce18af0a72
imrany <imranmat254@gmail.com>
2023-12-29 14:17:39 +0300

    ui update

--- web/src/routes/home.rs
+++ web/src/routes/home.rs
@@ -23,26 +23,30 @@ pub fn Home() -> impl IntoView {
             </nav>
 
             <div class="overflow-hidden fixed bottom-0 left-0 w-[200px] top-12 text-[13px] text-[#999999] bg-[#151515]">
-                <div class="flex flex-col m-3">
-                    <div class="text-[#e5e5e5] text-[11px] uppercase py-3">
+                <div class="flex flex-col my-3">
+                    <div class="text-[#e5e5e5] text-[11px] uppercase p-3">
                         <p>EXPLORER</p>
                     </div>
-                    <div class="flex flex-col">
-                        <p class="text-[#e5e5e5] text-[11px] uppercase py-1"> ">" zippy info</p>
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="text-[#e5e5e5] text-[11px] uppercase py-1"
-                        on:click=move |_| println!("hry")
-                        > ">" Telegram</p>
-                        <div class="pl-[8px] flex flex-col">
-                            <a href="/docs"> ">" Documentation</a>
-                        </div>
-                        <summary class="ml-3">My name</summary>
+                    <details class="flex flex-col">
+                        <summary class="text-[#e5e5e5] mx-[1px] px-3 text-[11px] uppercase py-1 cursor-pointer hover:text-white active:text-white focus:text-white focus:ring-1 focus:ring-violet-300">zippy info</summary>
                         <details>
-                            <p>My name is imran</p>
-                            <p>My name is imran</p>
+                            <summary class="hover:bg-[#999999] hover:bg-[#37373D] pl-6 pr-3 mx-[1px] cursor-pointer text-[#999999] active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name</summary>
+                            <div class="mt-[1px] flex flex-col text-[13px] text-[#999999]">
+                                <button class="hover:bg-[#999999] mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
+                                <button class="hover:bg-[#999999] mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
+                            </div>
                         </details>
-                    </div>
+                    </details>
+                    <details class="flex flex-col">
+                        <summary class="text-[#e5e5e5] mx-[1px] px-3 text-[11px] uppercase py-1 cursor-pointer hover:text-white active:text-white focus:text-white focus:ring-1 focus:ring-violet-300">Telegram</summary>
+                        <details>
+                            <summary class="hover:bg-[#3c3c3c]/35 pl-6 pr-3 mx-[1px] cursor-pointer text-[#999999] active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name</summary>
+                            <div class="mt-[1px] flex flex-col text-[13px] text-[#999999]">
+                                <button class="hover:bg-[#3c3c3c]/35 mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
+                                <button class="hover:bg-[#3c3c3c]/35 mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
+                            </div>
+                        </details>
+                    </details>
                 </div>
             </div>
         </div>
