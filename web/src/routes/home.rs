0ffe2212b5b6f901d63c8a1544cf51e996df6e4a
imrany <imranmat254@gmail.com>
2024-01-01 20:09:17 +0300

    sidebar ui update

--- web/src/routes/home.rs
+++ web/src/routes/home.rs
@@ -1,22 +1,22 @@
 use leptos::*;
 use leptos_meta::*;
-use leptos_icons::{
-    OcIcon::*,
-    FaIcon::*,
-    // ImIcon::*,
-    BsIcon::*,
-    BiIcon::*,
-    AiIcon::*,
-    Icon,
-};
+// use leptos_icons::{
+//     OcIcon::*,
+//     FaIcon::*,
+//     // ImIcon::*,
+//     BsIcon::*,
+//     BiIcon::*,
+//     AiIcon::*,
+//     Icon,
+// };
 
 #[path="../components/nav.rs"]
 mod nav;
 use nav::Nav;
 
-const ORANGE_ICON:&str =r#"
-    color: orange
-"#;
+// const ORANGE_ICON:&str =r#"
+//     color: orange
+// "#;
 
 #[component]
 pub fn Home() -> impl IntoView {
@@ -30,12 +30,12 @@ pub fn Home() -> impl IntoView {
                         <p>EXPLORER</p>
                     </div>
                     <details class="flex flex-col">
-                        <summary class="text-[#e5e5e5] mx-[1px] px-3 text-[11px] uppercase py-1 cursor-pointer hover:text-white active:text-white focus:text-white focus:ring-1 focus:ring-violet-300">zippy info</summary>
+                        <summary class="text-[#e5e5e5] mx-[1px] px-3 text-[11px] uppercase py-1 cursor-pointer hover:text-white active:text-white focus:text-white focus:ring-1 focus:ring-violet-300">Zippy info</summary>
                         <details>
-                            <summary class="hover:bg-[#999999] hover:bg-[#37373D] pl-6 pr-3 mx-[1px] cursor-pointer text-[#999999] active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name</summary>
+                            <summary class="hover:bg-[#3c3c3c]/35 pl-6 pr-3 mx-[1px] cursor-pointer text-[#999999] active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name</summary>
                             <div class="mt-[1px] flex flex-col text-[13px] text-[#999999]">
-                                <button class="hover:bg-[#999999] mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
-                                <button class="hover:bg-[#999999] mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
+                                <button class="hover:bg-[#3c3c3c]/35 mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
+                                <button class="hover:bg-[#3c3c3c]/35 mx-[1px] cursor-pointer active:bg-[#37373D] hover:text-white active:text-white focus:text-white focus:bg-[#37373D] focus:ring-1 focus:ring-violet-300">My name is imran</button>
                             </div>
                         </details>
                     </details>
@@ -51,12 +51,12 @@ pub fn Home() -> impl IntoView {
                     </details>
                 </div>
 
-                <Icon icon=Icon::from(AiCarryOutTwotone) style="color: green" />
-                <Icon icon=Icon::from(BiGraphql) width="2em" height="2em" style="color: green"/>
-                <Icon icon=Icon::from(BiGraphql) style=ORANGE_ICON/>
-                <Icon icon=Icon::from(FaBarsSolid) />
-                <Icon icon=Icon::from(BsFolder) />
-                <Icon icon=Icon::from(OcAlertSm) />
+                // <Icon icon=Icon::from(AiCarryOutTwotone) style="color: green" />
+                // <Icon icon=Icon::from(BiGraphql) width="2em" height="2em" style="color: green"/>
+                // <Icon icon=Icon::from(BiGraphql) style=ORANGE_ICON/>
+                // <Icon icon=Icon::from(FaBarsSolid) />
+                // <Icon icon=Icon::from(BsFolder) />
+                // <Icon icon=Icon::from(OcAlertSm) />
             </div>
         </div>
     }
