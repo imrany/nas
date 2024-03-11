0128bae1b2f089925c13b5c61b08a8e6aada5074
imrany <imranmat254@gmail.com>
2024-03-11 15:49:49 +0300

    fixed the toast issue

--- react/src/components/Footer.tsx
+++ react/src/components/Footer.tsx
@@ -20,7 +20,8 @@ export default function Footer(props:Props){
         }
     })
     function showToast(id:string){
-        document.getElementById(id)?.classList.toggle("none")
+        let toast=document.getElementById(id)
+        toast?.classList.contains("none")?toast?.classList.remove("none"):toast?.classList.add("none")
     }
     return(
         <footer className="fixed bottom-0 h-[25px] bg-blue-500 px-[7px] left-0 right-0">
@@ -38,20 +39,23 @@ export default function Footer(props:Props){
                         </Link>
                     </div>
                     <div className="flex items-center justify-center text-[13px] border-dotted border-l-[1px] border-gray-500">
-                        <button onClick={async ()=>{
-                            props.data.onlyFolders()
-                        }} onMouseOut={()=>showToast("folder_count")} onMouseLeave={()=>showToast("folder_count")} onMouseEnter={()=>showToast("folder_countr")} className="relative inline-block px-[10px] py-[2px] hover:bg-blue-400">
-                            <span id="folder_count" className="absolute none flex items-center justify-center text-gray-300 bg-[#252525] z-10 -mt-8 border-[1px] border-[var(--theme-gray)] -ml-6 h-[25px] min-w-[150px]">View folders : {folderCount.length}</span>
+                        <button onClick={()=>{
+                           props.data.onlyFolders()
+                        }} onMouseEnter={()=>showToast("folder_count")} onMouseLeave={()=>showToast("folder_count")} className="relative inline-block px-[15px] py-[2px] hover:bg-blue-400">
+                            <span id="folder_count" className="absolute none flex items-center justify-center text-gray-300 bg-[#252525] z-10 -mt-8 border-[1px] border-[var(--theme-gray)] -ml-10 h-[25px] min-w-[150px]">{folderCount.length!==0?(<p>View folders : {folderCount.length}</p>):(<p>No folder</p>)}</span>
                             <div className="flex gap-1 items-center ">
                                 <MdFolder/>
                                 <span>Folder: {folderCount.length}</span>
                             </div>
                         </button>
-                        <button onClick={async()=>{
+                        <button onClick={()=>{
                             props.data.onlyFiles()
-                        }} className="px-[10px] flex gap-1 items-center py-[2px] hover:bg-blue-400">
-                            <MdFileOpen/>
-                            <span>File: {fileCount.length}</span>
+                        }} onMouseEnter={()=>showToast("file_count")} onMouseLeave={()=>showToast("file_count")} className="relative inline-block px-[15px] py-[2px] hover:bg-blue-400">
+                            <span id="file_count" className="absolute none flex items-center justify-center text-gray-300 bg-[#252525] z-10 -mt-8 border-[1px] border-[var(--theme-gray)] -ml-10 h-[25px] min-w-[150px]">{fileCount.length!==0?(<p>View files : {fileCount.length}</p>):(<p>No file</p>)}</span>
+                            <div className="flex gap-1 items-center ">
+                                <MdFileOpen/>
+                                <span>File: {fileCount.length}</span>
+                            </div>
                         </button>
                         <Link to="/" onMouseEnter={()=>showToast("shared_folder")} onMouseLeave={()=>showToast("shared_folder")} className="relative inline-block px-[10px] py-[2px] hover:bg-blue-400">
                             <span id="shared_folder" className="absolute none flex items-center justify-center text-gray-300 bg-[#252525] z-10 -mt-8 border-[1px] border-[var(--theme-gray)] -ml-6 h-[25px] min-w-[150px]">View shared folders</span>
